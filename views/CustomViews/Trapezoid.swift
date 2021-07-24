5b935c576ce3410e9615ef57ccb7861151e448f9
vincio <airfoil_elector0b@icloud.com>
2021-07-24 23:37:09 +0800

    added Trapezoid View

---
+++ views/CustomViews/Trapezoid.swift
@@ -0,0 +1,39 @@
+//
+//  Trapezoid.swift
+//  views
+//
+//  Created by Vincio on 7/24/21.
+//
+
+import SwiftUI
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue } //for animation
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
+
+
+
+struct Trapezoid_Previews: PreviewProvider {
+    static var previews: some View {
+        Trapezoid(insetAmount: 50)
+            .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+    }
+}
