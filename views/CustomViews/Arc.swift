274e79bf5da9f5404d659de55b04c046fb069ab2
vincio <airfoil_elector0b@icloud.com>
2021-07-24 21:41:12 +0800

    added Arc View

---
+++ views/CustomViews/Arc.swift
@@ -0,0 +1,32 @@
+//
+//  Arc.swift
+//  views
+//
+//  Created by Vincio on 7/24/21.
+//
+
+import SwiftUI
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+}
+struct Arc_Previews: PreviewProvider {
+    static var previews: some View {
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(Color.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+    }
+}
