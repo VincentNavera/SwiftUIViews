057abb6e0c9ace4dcf7da1b95c0185bc4cfb4610
vincio <airfoil_elector0b@icloud.com>
2021-07-24 23:19:21 +0800

    added ColorCyclingCircle view

---
+++ views/CustomViews/ColorCyclingCircle.swift
@@ -0,0 +1,56 @@
+//
+//  ColorCyclingCircle.swift
+//  views
+//
+//  Created by Vincio on 7/24/21.
+//
+
+import SwiftUI
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct WithSliderView: View {
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+        }
+    }
+}
+
+struct ColorCyclingCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        WithSliderView()
+    }
+}
